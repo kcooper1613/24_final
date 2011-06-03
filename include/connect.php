d266d066f9372de25af697e2f9d2dc240b76adad
kcooper1613 <kcooper@friendscentral.org>
2011-06-03 07:14:47 -0700

    Edited include/connect.php via GitHub

--- include/connect.php
+++ include/connect.php
@@ -1,8 +1,8 @@
 <?php	
 	$server = "10.194.111.8";
-	$username = "user_a37d7be1";
-	$password = "xV2324&;p$^IyR";
-	$database = "db_a37d7be1";
+	$username = "user_a2a37514";
+	$password = "$zCWa3xQB85!YB";
+	$database = "db_a2a37514";
 	mysql_connect($server, $username, $password) or die ("Could not connect to server");
 	mysql_select_db($database) or die ("Could not connect to database");
 ?>
