a85d2794e6cad76d851ac03126ee233130ad1445
kcooper1613 <kcooper@friendscentral.org>
2011-06-03 07:26:28 -0700

    Edited posts/show.php via GitHub

--- posts/show.php
+++ posts/show.php
@@ -8,7 +8,7 @@
 		$query = "SELECT * FROM posts WHERE id = ".$myId;
 		$results = mysql_query($query) or die ("Could not get query");
 		$newPostContent = mysql_fetch_array($results);
-		print("<p class='showPage'>".$newPostContent['title']. " ".$newPostContent['content']." ".$newPostContent['tags']."</p>");
+		print("<p class='showPage'>Title: ".$newPostContent['title']. " Content: ".$newPostContent['content']." Tags: ".$newPostContent['tags']."</p>");
 		print('<p><a href="../posts/delete.php">DELETE</a></p>');
 		mysql_close();
 	?>
